566531bf381dfdefacd5df89d72a766deecea075
BahaaCaillou <tohami.bahaa93@gmail.com>
2024-08-23 15:38:42 +0200

    put profile

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { getUserProfile } from '../controllers/userController.js';
+import { getUserProfile, updateUserProfile } from '../controllers/userController.js';
 
 const router = express.Router();
 
 // Route pour récupérer le profil d'un utilisateur par son ID
 router.get('/profile/:id', getUserProfile);
-
+// Route pour mettre à jour le profil d'un utilisateur par son ID
+router.put('/profile/:id', updateUserProfile);
 export default router;
